3f3bc5563a1bc8d08f0780eabd5577aca494aa74
Fabrizio Pavone <fabriziopavone9985@gmail.com>
2025-03-07 11:02:31 +0100

    Esercizio_3

--- index.php
+++ index.php
@@ -41,14 +41,167 @@
 // }
 // }
 
-for ($i = 1; $i <= 100; $i++) {  
-    if ($i % 3 == 0 && $i % 5 == 0) {  
-        echo "Hackademy\n";
-    } elseif ($i % 3 == 0) { 
-        echo "Php\n";
-    } elseif ($i % 5 == 0) {  
-        echo "Javascript\n";
-    } else {
-        echo $i . "\n"; 
-    }
+// for ($i = 1; $i <= 100; $i++) {  
+//     if ($i % 3 == 0 && $i % 5 == 0) {  
+//         echo "Hackademy\n";
+//     } elseif ($i % 3 == 0) { 
+//         echo "Php\n";
+//     } elseif ($i % 5 == 0) {  
+//         echo "Javascript\n";
+//     } else {
+//         echo $i . "\n"; 
+//     }
+// };
+
+
+// ________________________________________________________________
+
+// $array = [10, 20, 12, 30, 40];
+
+// $filtro = array_filter($array, function($num) {
+//     return $num <= 30;
+
+// });
+
+// print_r($filtro);
+
+// $somma = array_sum($filtro);
+
+// echo($somma);
+
+// $mappa = array_map(function($numb){
+//         return $numb + 2;
+// },$filtro);
+
+// print_r($mappa);
+
+
+// ___________________________________________________________
+
+
+
+// ESERCIZIO 3
+
+// Rivedere la lezione
+// Ripetere l’esercizio del controllo password visto a lezione (da soli o rivedendo il video)
+// Implementare un metodo che faccia reinserire la password qualora anche una delle regole non fosse rispettate e che, invece, lo interrompa in caso di password accettata
+// visualizzare in console quale regola non e’ stata rispettata
+
+
+
+
+$password = readline("inserire password:");
+var_dump(strlen($password));
+
+
+
+
+function checkLenght($pwd){
+
+
+// CONTROLLA LA LUNGHEZZA DELLA PASSWORD - FUNZIONE : strlen
+
+
+if(strlen($pwd)>=10){               
+    echo "Password Corretta";
+    return true;
+}else{
+    echo "password non corretta";
+    return false;
 };
+
+
+
+}
+
+checkLenght($password);
+
+
+
+// CICLA E CONTROLLA SE ALENO UNA LETTERA E' IN MAIUSCOLO - FUNZIONE :  ctype_upper
+
+
+function checkUppercase($pwd){
+
+
+    
+    for ($i=0; $i < strlen($pwd) ; $i++) { 
+    
+        if(ctype_upper($pwd[$i])){
+            echo "ok \n" ;
+            break;
+            return true;
+
+           
+        }else{
+            echo "Ko \n";
+            return false;
+        }
+    }
+
+}
+
+checkUppercase($password);
+
+
+
+
+    // se trova almeno un numero
+
+
+function checkNumber($pwd){
+
+
+    for ($i=0; $i < strlen($pwd[$i]); $i++) { 
+       if (is_numeric($pwd[$i])) {
+           echo "numero presente \n";
+           break;
+           return true;
+       }else{
+           echo "numero non presente \n";
+           return false;
+       }
+    };
+
+
+}
+
+checkNumber($password);
+
+
+
+//  controlla se trova un carattere speciale
+
+function checkSpecial($pwd){
+    
+    $caratteri = [ "!", "?" , "@", "#"];
+
+
+    for ($i=0; $i <strlen($pwd) ; $i++) { 
+        if(in_array($pwd[$i], $caratteri)){
+            echo "Carattere speciale trovato \n";
+            return true;
+            
+    
+        }else{
+            echo "carattere speciale non trovato \n";
+            return false;
+        }
+        
+    }
+
+
+}
+
+checkSpecial($password);
+
+
+
+
+
+
+
+
+
+
+
